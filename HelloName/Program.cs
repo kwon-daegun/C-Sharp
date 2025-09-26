3852e381daab5be5b4e315ee32aff04e93a95147
kwon daegun <kalki127001@gmail.com>
2025-09-27 03:05:46 +0530

    greeting user program
    takes user input and greets hello with their name

---
+++ HelloName/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace HelloName
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please enter your name: "); // prompt user for input
+
+            var name = Console.ReadLine(); // store user input
+            Console.WriteLine($"Hello, {name}!"); // string interpolation
+        }
+    }
+}
